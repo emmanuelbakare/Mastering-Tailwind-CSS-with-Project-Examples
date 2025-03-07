b0b3151d9b7166afb4cd6d7b2e1cdf12bd4368fb
Emmanuel Bakare <emmanuelbakare@yahoo.com>
2025-03-07 20:16:45 +0100

    Project 01 update 07-03-2025

---
+++ 09-Advanced-Features/06-external-thirdparty-plugins/node_modules/flowbite-datepicker/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./js/**/*.js"],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    // require('flowbite/plugin')
+  ],
+  corePlugins: {
+    preflight: false,
+  }
+}
